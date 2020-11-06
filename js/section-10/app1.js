3120f7b495051a865369c70829aa06a3b1ad756a
Brian Bycynski <high5bri@hotmail.com>
2020-11-06 03:23:18 -0500

    started section 10

---
+++ js/section-10/app1.js
@@ -0,0 +1,22 @@
+//
+//
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 190) {
+  // numPassengers = numPassengers || 1;
+  // price = price || 190;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 100, 120);
